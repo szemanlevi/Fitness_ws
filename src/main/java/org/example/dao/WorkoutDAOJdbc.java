597693bfdd560bdb4fd3941b9e2336bd4fb3cd4a
szemanlevi <szemanlevente@gmail.com>
2023-03-01 10:21:29 +0100

    workoutdaojdbc CRUD done

--- src/main/java/org/example/dao/WorkoutDAOJdbc.java
+++ src/main/java/org/example/dao/WorkoutDAOJdbc.java
@@ -1,23 +1,22 @@
 package org.example.dao;
 
 import org.example.model.Workout;
-import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class WorkoutDAOJdbc implements WorkoutDAO {
 
-    JdbcTemplate jdbcTemplate;
-
-    WorkoutRowMapper workoutRowMapper;
     private static final Logger logger = LoggerFactory.getLogger(WorkoutDAOJdbc.class);
+    JdbcTemplate jdbcTemplate;
+    WorkoutRowMapper workoutRowMapper;
 
     @Autowired
     public WorkoutDAOJdbc(JdbcTemplate jdbcTemplate, WorkoutRowMapper workoutRowMapper) {
@@ -43,21 +42,66 @@ public class WorkoutDAOJdbc implements WorkoutDAO {
 
     @Override
     public void deleteWorkout(int id) {
-
+        final String sql = """
+                DELETE FROM workout
+                WHERE id = ?
+                """;
+        int log = jdbcTemplate.update(sql, id);
+        if (log == 1) {
+            logger.info("workout with id = {} deleted", id);
+        } else {
+            logger.warn("problem occured while deleting workout");
+        }
     }
 
     @Override
     public Workout getWorkout(int id) {
-        return null;
+        final String sql = """
+                SELECT * FROM workout
+                WHERE id = ?
+                """;
+        Workout workout = new Workout();
+        try {
+            workout = jdbcTemplate.queryForObject(sql, new Object[]{id}, workoutRowMapper);
+
+        } catch (DataAccessException e) {
+            logger.warn("id not found");
+        } catch (Exception e) {
+            logger.warn("unexpected error occured");
+        }
+        return workout;
     }
 
     @Override
     public void updateWorkout(int id, Workout workout) {
-
+        final String sql = """
+                UPDATE workout
+                SET name = ?,
+                duration = ?,
+                difficulty = ?
+                WHERE id = ?
+                """;
+        int log = jdbcTemplate.update(sql, workout.getName(), workout.getDuration(), workout.getDifficulty(), id);
+        if (log == 1) {
+            logger.info("workout with id = {} updated", id);
+        } else {
+            logger.warn("problem occured while updating workout");
+        }
     }
 
     @Override
     public List<Workout> listAllWorkouts() {
-        return null;
+        final String sql = """
+                SELECT * FROM workout
+                """;
+        List<Workout> workouts = new ArrayList<>();
+        try {
+            workouts = jdbcTemplate.query(sql, workoutRowMapper);
+        } catch (DataAccessException e) {
+            logger.warn("workouts not found");
+        } catch (Exception e) {
+            logger.warn("unexpected error occured");
+        }
+        return workouts;
     }
 }
