29f08ef99117583713279e43c5754e44c2547f59
szemanlevi <szemanlevente@gmail.com>
2023-03-01 10:34:51 +0100

    main and schema fixed

--- src/main/java/org/example/FitnessApplication.java
+++ src/main/java/org/example/FitnessApplication.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.example.dao.WorkoutDAO;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class FitnessApplication {
+    private WorkoutDAO workoutDAO;
+
+    public FitnessApplication(WorkoutDAO workoutDAO) {
+        this.workoutDAO = workoutDAO;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(FitnessApplication.class, args);
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,13 @@
-CREATE TABLE workout (
+CREATE TABLE IF NOT EXISTS workout (
                          id SERIAL PRIMARY KEY,
                          name VARCHAR(50) NOT NULL,
                          duration INT NOT NULL,
-                         difficulty VARCHAR(10) NOT NULL
+                         difficulty VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE exercise (
+CREATE TABLE IF NOT EXISTS exercise (
                           id SERIAL PRIMARY KEY,
                           name VARCHAR(50) NOT NULL,
                           calories_burned INT NOT NULL,
-                          muscle_group VARCHAR(10) NOT NULL
+                          muscle_group VARCHAR(20) NOT NULL
 );
